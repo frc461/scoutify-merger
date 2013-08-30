c2a42e3dd40ce9b957ce8f643a1f7e842c218f2c
Kristofer Rye <kristofer.rye@gmail.com>
2013-08-30 14:42:50 -0400

    adding argument-parsing header and file, preparations for JSON parsing within JSON objects.

---
+++ src/argument.h
@@ -0,0 +1,8 @@
+#ifndef ARGUMENT_H
+#define ARGUMENT_H
+
+typedef struct argument {
+	
+} argument_t;
+
+#endif
--- src/json.h
+++ src/json.h
@@ -1,4 +1,9 @@
 #ifndef JSON_H
 #define JSON_H
 
+typedef struct json_data {
+	unsigned int match_number;
+	unsigned int team_number;
+} json_data_t;
+
 #endif
