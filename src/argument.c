e6022dd6300dbd5f98ba7fcf23925ec1d1956f78
Kristofer Rye <kristofer.rye@gmail.com>
2013-08-31 14:26:05 -0400

    fixes like 20 compile errors

--- src/argument.c
+++ src/argument.c
@@ -1,6 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 #include <src/argument.h>
 
-argument_t *_root_argument_ = argument_new();
+argument_t *_root_argument_ = NULL;
 
 argument_t *_argument_new_()
 { argument_t *ret = malloc(sizeof(argument_t));
@@ -10,7 +13,7 @@ argument_t *_argument_new_()
 argument_t *argument_new_from_data(int argc, char *argv)
 { argument_t *ret = _argument_new_();
 	ret->value = argv;
-	ret->argc = argc;
+	ret->index = argc;
 
 	return ret;
 }
@@ -24,11 +27,11 @@ void argument_insert_after_argument(argument_t *argument, argument_t *to_insert)
 {
 	if(argument->next != NULL) {
 		printf("insertion detected previous next @0x%x w/ parent @0x%x\n", argument->next, argument);
-		printf("%4s(we'll make a copy and then insert that as the next of the insertion)\n". "");
+		printf("%4s(we'll make a copy and then insert that as the next of the insertion)\n", "");
 
 		argument_t *previous_next = argument->next;
 		argument->next = to_insert;
-		argument->next->next = previous_next();
+		argument->next->next = previous_next;
 		
 	}
 	argument->next = to_insert;
--- src/argument.h
+++ src/argument.h
@@ -5,7 +5,7 @@ typedef struct argument {
 	char *value;
 	int index;
 	
-	argument *next;
+	struct argument *next;
 } argument_t;
 
 extern argument_t *_root_argument_;
@@ -14,7 +14,7 @@ argument_t *_argument_new_();
 argument_t *argument_new_from_data(int argc, char *argv);
 argument_t *argument_new_from_data_after_argument(int argc, char *argv, argument_t *argument);
 
-void argument_insert_after_argument(argument_t *argument);
+void argument_insert_after_argument(argument_t *argument, argument_t *to_insert);
 
 argument_t *argument_last_in_linked_list();
 
