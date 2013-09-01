beed23d990c444f93aa57520faefe9a7697d2036
Kristofer Rye <kristofer.rye@gmail.com>
2013-09-01 19:37:00 -0400

    changing something

--- src/team.c
+++ src/team.c
@@ -6,13 +6,16 @@
 /*
  * Simply creates, allocates, and returns an empty team structure (pointers inside may not be initialized!)
  */
+
 team_t *team_new()
 { team_t *ret = malloc(sizeof(team_t));
 	return ret;
 }
+
 /*
  * Creates a new team struct and populates it with the arguments given as the data
  */
+
 team_t *team_new_from_data(unsigned int number, char name[128], match_t **matches, unsigned int num_matches)
 { team_t *ret = team_new();
 
