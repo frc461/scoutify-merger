a375a0ddc849de948ada15f06bd3d089b588c2a2
Christopher Cooper <christopher.c.cooper@gmail.com>
2013-09-02 11:57:27 -0400

    licensing

--- src/display.h
+++ src/display.h
@@ -1,3 +1,22 @@
+/*
+    Copyright Christopher Cooper and Kristofer Rye 2013
+
+    This file is part of scoutify-hub.
+
+    scoutify-hub is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    scoutify-hub is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with scoutify-hub.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #ifndef DISPLAY_H
 #define DISPLAY_H
 
