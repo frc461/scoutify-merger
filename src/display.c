91d0a5e738902f9ea0f39f10176d49e15d1e16d3
Christopher Cooper <christopher.c.cooper@gmail.com>
2013-09-02 11:14:54 -0400

    fix incorect database usage

--- src/display.c
+++ src/display.c
@@ -7,9 +7,7 @@
 
 void database_display()
 {
-	team_t **db = _unrestricted_db_();
-	
-	for(int i = 0; i < _database_num_elements_(); i += 1) {
+	for(int i = 0; database_get_nth_element(i); i += 1) {
 		team_t *curteam = database_get_nth_element(i);
 		printf("%d: Team %8lu with sort-value %8d\n", i, curteam->number, curteam->value);
 	}
@@ -17,11 +15,9 @@ void database_display()
 
 void database_display_table_mode()
 {
-	team_t **db = _unrestricted_db_();
-
 	printf("%6s | %16s | %12s\n", "", "TEAM NUMBER", "SORT VALUE");
 	
-	for(int i = 0; i < _database_num_elements_(); i += 1) {
+	for(int i = 0; database_get_nth_element(i); i += 1) {
 		team_t *curteam = database_get_nth_element(i);
 		
 		printf("%6d | %16u | %12d\n", i, curteam->number, curteam->value);
