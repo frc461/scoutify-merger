72f9dc53a848ff596c7dafccbfc3b8ccdb57e8e2
Christopher Cooper <christopher.c.cooper@gmail.com>
2013-09-02 11:40:40 -0400

    even better

--- src/display.c
+++ src/display.c
@@ -7,8 +7,8 @@
 
 void database_display()
 {
-	for(int i = 0; database_get_nth_element(i); i += 1) {
-		team_t *curteam = database_get_nth_element(i);
+	team_t *curteam;
+	for(int i = 0; curteam = database_get_nth_element(i); i += 1) {
 		printf("%d: Team %8lu with sort-value %8d\n", i, curteam->number, curteam->value);
 	}
 }
@@ -17,8 +17,8 @@ void database_display_table_mode()
 {
 	printf("%6s | %16s | %12s\n", "", "TEAM NUMBER", "SORT VALUE");
 	
-	for(int i = 0; database_get_nth_element(i); i += 1) {
-		team_t *curteam = database_get_nth_element(i);
+	team_t *curteam;
+	for(int i = 0; curteam = database_get_nth_element(i); i += 1) {
 		
 		printf("%6d | %16u | %12d\n", i, curteam->number, curteam->value);
 	}
