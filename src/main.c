acddb4bebff2339422ba4203f2c8288f6fa24822
Kristofer Rye <kristofer.rye@gmail.com>
2013-09-04 13:08:54 -0400

    add stuff

--- src/main.c
+++ src/main.c
@@ -35,7 +35,7 @@ int main(int argc, char *argv[])
 	char **argument_values = argument_get_values_from_system_list();
 	int arguments = argument_get_number_of_elements_in_system_list() - 1;
 
-	populate_db_from_file("raw/test.json");
+	populate_db_from_file("./raw/test.json");
 
 	for(int i = 0; i < arguments; i += 1) {
 		load_dot_scoutify(argument_values[i]);
@@ -48,7 +48,7 @@ int main(int argc, char *argv[])
 	database_display();
 	database_display_table_mode();
 
-	write_db_to_file("raw/test.json");
+	write_db_to_file("./raw/test.json");
 
 	dump_database_to_csv_file("database.csv");
 	
