e699ad9002c27b35ee07228ace4ec05e895fd58f
Kristofer Rye <kristofer.rye@gmail.com>
2014-01-30 18:54:10 -0500

    Change the block comment style in team.h

--- src/team.h
+++ src/team.h
@@ -1,21 +1,21 @@
 /*
-    Copyright Christopher Cooper and Kristofer Rye 2013
-
-    This file is part of scoutify-merger.
-
-    scoutify-merger is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    scoutify-merger is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with scoutify-merger.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ * Copyright Christopher Cooper and Kristofer Rye 2013
+ *
+ * This file is part of scoutify-merger.
+ *
+ * scoutify-merger is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * scoutify-merger is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with scoutify-merger.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #ifndef TEAM_H
 #define TEAM_H
